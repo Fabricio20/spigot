6fc63bf5cc14e6764f251eca51e17d098e021070
Fabricio Winter <git@notfab.net>
2021-12-06 20:19:12 -0300

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Downloading BuildTools..."
+curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+
+echo "Running BuildTools.."
+java -jar BuildTools.jar --rev 1.17
